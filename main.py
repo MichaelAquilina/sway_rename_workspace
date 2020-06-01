a07b6b0556d3a9d4789e46e740b173dd6ed1cc0a
Michael Aquilina <michaelaquilina@gmail.com>
2020-06-01 10:35:47 +0100

    Improvements to displaying icons

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import re
 import shutil
@@ -5,45 +6,55 @@ from typing import List
 
 from i3ipc import Connection, Event
 
+logger = logging.getLogger(__name__)
+
+# See https://fontawesome.com/cheatsheet
+# for a quick way to search appropriate icons
 GLYPH_MAP = {
     "kitty": "" ,
-    "Monitor": "",
-    "firefox": "",
-    "rhythmbox": "",
-    "Spotify": "",
-    "Network": "  ",
-    "Calculator": "  ",
-    "Default": "  ",
+    "firefox": "",
+    "rhythmbox": "",
+    "Spotify": "",
+    "steam": "",
+    "zoom": "",
 }
 
-def rename_workspaces(i3, event):
+def rename_workspaces(i3, _):
     for workspace in i3.get_tree().workspaces():
-        set_workspace_name(i3, workspace)
+        try:
+            set_workspace_name(i3, workspace)
+        except Exception:
+            logger.exception("Unexpected error")
 
 
 def set_workspace_name(i3, workspace):
-    icons = set()
+    icons = []
     for node in workspace.leaves():
         # X windows
         if node.app_id is None:
-            key = node.ipc_data["window_properties"]["class"]
+            key = node.ipc_data.get("window_properties", {}).get("class")
         else:
             key = node.app_id
 
         print(key)
 
         if key in GLYPH_MAP:
-            icons.add(GLYPH_MAP[key])
+            icons.append(GLYPH_MAP[key])
 
 
     old = workspace.name
-    new = f"{workspace.num} " + " ".join(sorted(icons))
+    if icons:
+        new = f"{workspace.num} " + " ".join(icons) + " "
+    else:
+        new = f"{workspace.num}"
 
     i3.command(f"rename workspace '{old}' to '{new}'")
 
 
 def main():
     i3 = Connection()
+
+    rename_workspaces(i3, None)
 
     i3.on(Event.WINDOW_NEW, rename_workspaces)
     i3.on(Event.WINDOW_CLOSE, rename_workspaces)
